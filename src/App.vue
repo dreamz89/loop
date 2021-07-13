f51c44288330b99506b336e7246545811a5f3a24
Jocelyn Tan <jocelyntan89@hotmail.com>
2021-07-13 19:27:06 +0200

    delete HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,18 @@
   <div id="app">
     <div class="container">
       <Navigation />
-      <img alt="Vue logo" src="./assets/logo.png" />
-      <HelloWorld msg="Welcome to Your Vue.js App" />
       <Footer />
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
 import Navigation from "./components/Navigation.vue"
 import Footer from './components/Footer.vue'
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     Navigation,
     Footer,
   },
